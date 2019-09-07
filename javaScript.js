a6b1d9e39c96dc760f7413799700cc99d998ed67
Eliasyoussef47 <d-s-a00@hotmail.com>
2019-09-07 21:15:13 +0200

    files organized

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,25 @@
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			$('#blah')
+				.attr('src', e.target.result);
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
+}
 
-// var answer = prompt ("");
-//
-//
-//   function cloneItem() {
-//        var item = document.getElementById("item");
-//        var list = document.getElementById("list");
-//        var clonedItem = item.cloneNode(true);
-//        list.appendChild(clonedItem);
-//    }
-//
-//    for(var i = 0; i < answer; i++) {
-//      cloneItem();
-//    }
+function DownloadFile() {
+	file = document.getElementById("fileSelector").files[0];
+
+	var blob = new Blob([file], { type: "application/png" });
+
+	var objectURL = window.URL.createObjectURL(blob);
+
+	var link = document.createElement('a');
+	link.href = objectURL;
+	link.download = "myFileName.png";
+	link.click();
+	window.URL.revokeObjectURL(objectURL);
+	link.remove();
+}
