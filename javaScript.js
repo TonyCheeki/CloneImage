c3d6ddb3c9f1c50d36a2c9db6372b4f4deb2ba59
Eliasyoussef47 <d-s-a00@hotmail.com>
2019-09-07 23:30:56 +0200

    JSZIP gebruikt om de geclonde bestanden in een zip te zetten

--- javaScript.js
+++ javaScript.js
@@ -2,24 +2,30 @@ function readURL(input) {
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
 		reader.onload = function (e) {
-			$('#blah')
-				.attr('src', e.target.result);
+			$('#blah').attr('src', e.target.result);
 		};
 		reader.readAsDataURL(input.files[0]);
 	}
 }
 
 function DownloadFile() {
-	file = document.getElementById("fileSelector").files[0];
-
-	var blob = new Blob([file], { type: "application/png" });
-
-	var objectURL = window.URL.createObjectURL(blob);
-
+	var file = document.getElementById("fileSelector").files[0];
+	var uploadedFileBlob = new Blob([file], { type: "image/jpeg" });
+	var objectURL;
 	var link = document.createElement('a');
-	link.href = objectURL;
-	link.download = "myFileName.png";
-	link.click();
-	window.URL.revokeObjectURL(objectURL);
-	link.remove();
+	var zip = new JSZip();
+	var filesNamesArr = document.getElementById("filesNames").value.split(",");// als je een textarea gebruikt moet je innerText gebruiken inplaats van value
+	filesNamesArr.forEach(function (element) {
+		zip.file(element + ".jpg", uploadedFileBlob);
+	});
+	zip.generateAsync({type: "blob"}).then(function (blob) {
+		objectURL = window.URL.createObjectURL(blob);
+		link.href = objectURL;
+		link.download = "clonedImages.zip";
+		link.click();
+		window.URL.revokeObjectURL(objectURL);
+		link.remove();
+	});
 }
+
+//TODO: regex toevoegen voor kommas
